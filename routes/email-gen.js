7df607bc42f9e021304a661a694a7ccaa7f5a640
Vasilis Souvatzis <vasouv@gmail.com>
2023-02-19 22:44:55 +0200

    endpoints
    endpoints for getAllEmails, deleteEmail and more checks

--- routes/email-gen.js
+++ routes/email-gen.js
@@ -2,16 +2,34 @@ var express = require('express');
 var emailService = require('../services/email-service');
 var router = express.Router();
 
-// GET generated email
-router.get('/', function(req, res) {
-
+// POST generated email
+router.post('/', function(req, res) {
     try {
-        let email = emailService.generateEmail(req.query.name, req.query.surname);
+        let email = emailService.generateEmail(req.query.name, req.query.surname, req.query.domain);
         res.send(email);
     } catch (error) {
         res.status(400).send(error.message);
     }
+});
 
+// GET all generated emails
+router.get('/', function(req, res) {
+
+    let emails = emailService.getAllEmails();
+    let noOfEmails = emails.length;
+
+    let emailDB = {
+        "size": noOfEmails,
+        "emails": emails
+    }
+
+    res.json(emailDB);
+    
+});
+
+// DELETE generated email
+router.delete('/', function(req, res) {
+    res.send(emailService.deleteEmail(req.query.email));
 });
 
 module.exports = router;
--- services/email-service.js
+++ services/email-service.js
@@ -1,27 +1,50 @@
 const EMAILS_DB = []
 
-function generateEmail(name, surname) {
+function generateEmail(name, surname, domain) {
 
-    if (!name || !surname) {
-        throw new Error("Name or Surname is blank");
+    if (!name || !surname || !domain) {
+        throw new Error("Name, Surname or Domain is blank");
     }
 
+    validateDomain(domain);
+
     let nameL = name.toLowerCase();
-    let surnameL = surname.toLowerCase();
-    let email = `${nameL}.${surnameL}@mail.com`;
+    let surnameL = surname.toUpperCase();
+    let email = `${nameL}.${surnameL}@${domain}`;
 
     if (emailExists(email)) {
         throw new Error("Email already exists");
     }
 
     EMAILS_DB.push(email);
-    console.log(EMAILS_DB);
 
     return email;
+}
+
+function validateDomain(domain) {
+    if (!domain.includes('.')) {
+        throw new Error("Domain must be xxx.ccc");
+    }
+    return true;
 }
 
 function emailExists(email) {
     return EMAILS_DB.includes(email);
 }
 
+function getAllEmails() {
+    return EMAILS_DB;
+}
+
+function deleteEmail(email) {
+    if (!emailExists(email)) {
+        return false;
+    }
+    let index = EMAILS_DB.indexOf(email);
+    EMAILS_DB.splice(index, 1);
+    return true;
+}
+
 module.exports.generateEmail = generateEmail;
+module.exports.getAllEmails = getAllEmails;
+module.exports.deleteEmail = deleteEmail;
